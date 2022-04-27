be150f20b7fc5b24b0a11584a11b6bb0474e468f
Bea Patnugot <97040197+Shobea@users.noreply.github.com>
2022-04-27 15:11:36 +0800

    1/12 UI

---
+++ app/src/main/java/com/example/group1_capstone/View/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.example.group1_capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
