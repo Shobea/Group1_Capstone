680f0a82c90a23ec05ff32194294a7fda83c1981
Bea Patnugot <97040197+Shobea@users.noreply.github.com>
2022-05-10 12:51:04 +0800

    9/12 UI

--- app/src/main/java/com/example/group1_capstone/View/ResortsList.java
+++ app/src/main/java/com/example/group1_capstone/View/ResortsList.java
@@ -1,12 +1,16 @@
 package com.example.group1_capstone.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.group1_capstone.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ResortsList extends AppCompatActivity {
 
@@ -17,5 +21,27 @@ public class ResortsList extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_resorts_list);
+
+        BottomNavigationView navBar=findViewById(R.id.navigation_bar2);
+        navBar.setSelectedItemId(R.id.notification);
+        navBar.setSelectedItemId(R.id.heart);
+        navBar.setSelectedItemId(R.id.book);
+
+        navBar.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case  R.id.notification:
+                        startActivity(new Intent(getApplicationContext(),RestaurantsList.class));
+                        break;
+                    case R.id.heart:
+                        startActivity(new Intent(getApplicationContext(),BookmarkList.class));
+                        break;
+                    case R.id.book:
+                        startActivity(new Intent(getApplicationContext(),CreateAccountScreen.class));
+                        break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/group1_capstone/View/RestaurantsList.java
+++ app/src/main/java/com/example/group1_capstone/View/RestaurantsList.java
@@ -1,12 +1,16 @@
 package com.example.group1_capstone.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.group1_capstone.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class RestaurantsList extends AppCompatActivity {
 
@@ -17,5 +21,27 @@ public class RestaurantsList extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_restaurants_list);
+
+        BottomNavigationView navBar=findViewById(R.id.navigation_bar2);
+        navBar.setSelectedItemId(R.id.notification);
+        navBar.setSelectedItemId(R.id.heart);
+        navBar.setSelectedItemId(R.id.book);
+
+        navBar.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case  R.id.notification:
+                        startActivity(new Intent(getApplicationContext(),PromosScreen.class));
+                        break;
+                    case R.id.heart:
+                        startActivity(new Intent(getApplicationContext(),BookmarkList.class));
+                        break;
+                    case R.id.book:
+                        startActivity(new Intent(getApplicationContext(),CreateAccountScreen.class));
+                        break;
+                }
+            }
+        });
     }
-}
+    }
