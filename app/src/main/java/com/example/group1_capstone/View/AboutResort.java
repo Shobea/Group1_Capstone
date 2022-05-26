a4a925bfe14106b5bb4d3024a2cfcfdfb8c643a6
Bea Patnugot <97040197+Shobea@users.noreply.github.com>
2022-05-26 13:16:58 +0800

    12/12 UI

--- app/src/main/java/com/example/group1_capstone/View/AboutResort.java
+++ app/src/main/java/com/example/group1_capstone/View/AboutResort.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.group1_capstone.R;
 
@@ -24,6 +25,9 @@ public class AboutResort extends AppCompatActivity {
     }
     public void buttonFunction(View view){
         switch (view.getId()){
+            case R.id.code:
+                Toast.makeText(this, "10% off when you book", Toast.LENGTH_LONG).show();
+                break;
             case R.id.button13:
                 finish();
                 break;
@@ -37,7 +41,9 @@ public class AboutResort extends AppCompatActivity {
                 overridePendingTransition(1,1);
                 break;
             case R.id.bookResort:
-                startActivity(new Intent(this, BookResort.class));
+                Intent next=new Intent(getApplicationContext(),BookResort.class);
+                next.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(next);
         }
     }
 }
--- app/src/main/java/com/example/group1_capstone/View/BookResort.java
+++ app/src/main/java/com/example/group1_capstone/View/BookResort.java
@@ -3,25 +3,36 @@ package com.example.group1_capstone.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.group1_capstone.R;
 
-public class BookResort extends Activity {
+public class BookResort extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_resort);
+    }
 
-        DisplayMetrics displayMetrics= new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int width=displayMetrics.widthPixels;
-        int height=displayMetrics.heightPixels;
-
-        getWindow().setLayout((int) (width*.9),(int) (height*.5));
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.cancel:
+                finish();
+                break;
+            case R.id.bookResort2:
+                Toast.makeText(this, "Booking details has been sent to your e-mail", Toast.LENGTH_LONG).show();
+                Intent next=new Intent(getApplicationContext(),AboutResort.class);
+                next.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(next);
+                break;
+        }
     }
 }
